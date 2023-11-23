a4247ec087ebfa449866fe3900f0067ac9e24c1e
Kristofer Rogue <75965259+Kristofer-Rogue@users.noreply.github.com>
2023-11-23 12:43:02 +0700

    added the register

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap5
 from decouple import config
 from routes import main_bp
+from os import urandom
 
 
 def create_app() -> Flask:
     """
-    Create and configure the Flask application.
+    Create and configure the Flask applicat ion.
 
     Returns:
         Flask: The configured Flask application.
@@ -21,6 +22,7 @@ def create_app() -> Flask:
     db_password = config('DB_PASSWORD')
     db_name = config('DB_NAME')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_password}@{db_host}/{db_name}'
+    app.config['SECRET_KEY'] = urandom(32)
 
     return app
 
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, session, redirect, request, flash
 from user import User
 from database import db
 
@@ -18,3 +18,34 @@ def project(project_id):
 @main_bp.route('/')
 def index():
     return render_template('timer.html')
+
+
+@main_bp.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@main_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if session.get('user'):
+        return redirect('/')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirmPassword']
+
+        if not email or not password:
+            flash('Email and password are required', category='danger')
+            return render_template('register.html')
+        if User.query.filter_by(_email=email).first():
+            flash('Email already exists', category='danger')
+            return render_template('register.html')
+        if password != confirm_password:
+            flash('Passwords do not match', category='danger')
+            return render_template('register.html')
+
+        user = User(email, password)
+        session['user'] = user.get_id()
+        session['login'] = True
+        return redirect('/')
+    return render_template('register.html')
